9b89b5a8d361cfbd4c28d1fea9759568756f39cb
quanghuy02022002 <hoanganhzt1506@gmail.com>
2025-03-22 02:31:06 +0700

    1
    Create BossLV2

---
+++ Knight_J97/Assets/Scripts/BossEnemy.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossEnemy : MonoBehaviour
+{
+    [Header("Combat Parameters")]
+    [SerializeField] private float attackCooldown = 1f;
+    [SerializeField] private float range = 2f;
+    [SerializeField] private float colliderDistance = 2.1f;
+    [SerializeField] private int damage = 40;
+    [SerializeField] private BoxCollider2D boxCollider;
+
+    private float cooldownTimer = Mathf.Infinity;
+    private Animator animator;
+    private BossPatrol bossPatrol;
+    private Health playerHealth;
+    private bool isDead = false;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+        bossPatrol = GetComponentInParent<BossPatrol>();
+    }
+
+    void Update()
+    {
+        if (isDead) return;
+
+        cooldownTimer += Time.deltaTime;
+
+        if (PlayerInSight() && playerHealth != null && !playerHealth.isDead)
+        {
+            if (cooldownTimer >= attackCooldown)
+            {
+                cooldownTimer = 0;
+                animator.SetTrigger("Attack");
+            }
+
+            if (bossPatrol != null)
+            {
+                bossPatrol.StopPatrol(); // Dừng di chuyển khi chuẩn bị tấn công
+            }
+        }
+        else
+        {
+            if (bossPatrol != null)
+            {
+                bossPatrol.ResumePatrol(); // Tiếp tục tuần tra khi không thấy player
+            }
+        }
+    }
+
+    bool PlayerInSight()
+    {
+        RaycastHit2D hit = Physics2D.BoxCast(
+            boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
+            new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z),
+            0, Vector2.left, 0, LayerMask.GetMask("Player"));
+
+        if (hit.collider != null)
+        {
+            playerHealth = hit.transform.GetComponent<Health>();
+        }
+
+        return hit.collider != null;
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(
+            boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
+            new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z));
+    }
+
+    // Gọi trong Animation Event để gây sát thương
+    public void DamagePlayer()
+    {
+        if (PlayerInSight())
+        {
+            playerHealth.PlayerTakeDamage(damage);
+        }
+    }
+
+    void Die()
+    {
+        isDead = true;
+        animator.SetTrigger("Die");
+        Destroy(gameObject, 3f); // Xóa boss sau 3 giây
+    }
+}
---
+++ Knight_J97/Assets/Scripts/BossPatrol.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossPatrol : MonoBehaviour
+{
+    [Header("Patrol Points")]
+    [SerializeField] Transform leftEdge;
+    [SerializeField] Transform rightEdge;
+
+    [Header("Boss")]
+    [SerializeField] Transform boss;
+
+    [Header("Movement Parameters")]
+    [SerializeField] float speed;
+    private Vector3 initScale;
+    private bool movingLeft;
+
+    [Header("Idle")]
+    [SerializeField] private float idleDuration;
+    private float idleTimer;
+
+    [Header("Boss Animator")]
+    [SerializeField] private Animator animator;
+
+    void Awake()
+    {
+        initScale = boss.localScale;
+    }
+
+    void OnDisable()
+    {
+        animator.SetBool("isMoving", false);
+    }
+
+    void Update()
+    {
+        if (boss != null)
+        {
+            if (movingLeft)
+            {
+                if (boss.position.x >= leftEdge.position.x)
+                {
+                    MoveInDirection(-1);
+                }
+                else
+                {
+                    DirectionChange();
+                }
+            }
+            else
+            {
+                if (boss.position.x <= rightEdge.position.x)
+                {
+                    MoveInDirection(1);
+                }
+                else
+                {
+                    DirectionChange();
+                }
+            }
+        }
+    }
+
+    void DirectionChange()
+    {
+        animator.SetBool("isMoving", false);
+        idleTimer += Time.deltaTime;
+        if (idleTimer > idleDuration)
+        {
+            movingLeft = !movingLeft;
+            idleTimer = 0;
+        }
+    }
+
+    void MoveInDirection(int direction)
+    {
+        idleTimer = 0;
+        animator.SetBool("isMoving", true);
+        boss.localScale = new Vector3(Mathf.Abs(initScale.x) * direction, initScale.y, initScale.z);
+        boss.position = new Vector3(boss.position.x + Time.deltaTime * direction * speed, boss.position.y, boss.position.z);
+    }
+
+    public void StopPatrol()
+    {
+        animator.SetBool("isMoving", false);
+        this.enabled = false; // Dừng tuần tra khi boss chuẩn bị tấn công
+    }
+
+    public void ResumePatrol()
+    {
+        this.enabled = true; // Tiếp tục tuần tra khi không phát hiện player
+    }
+}
