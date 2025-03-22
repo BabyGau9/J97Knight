dfe76ac527103cdcfd180c2bd0d2026e3953e3b4
BabyGau9 <caodung832003@gmail.com>
2025-03-22 15:11:23 +0700

    Add new assets and animations

---
+++ Knight_J97/Assets/Scripts/DragonMovement.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class DragonMovement : MonoBehaviour
+{
+   
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
